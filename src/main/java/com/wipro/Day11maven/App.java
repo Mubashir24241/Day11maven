6cecede5da241b96e362c8ebf104994757446314
Mubashir24241 <mubashir.ahmad.cse.2020@miet.ac.in>
2025-05-19 10:09:40 +0530

    first commit

--- src/main/java/com/wipro/Day11maven/App.java
+++ src/main/java/com/wipro/Day11maven/App.java
@@ -2,6 +2,6 @@ package com.wipro.Day11maven;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello MUBASHIR");
+        System.out.println("Hello Mubashir!");
     }
 }
